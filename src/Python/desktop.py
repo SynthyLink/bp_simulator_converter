32099496b2d99e96eb508b3b741b78beb1db4c1c
nikolay.ivankov <nikolay.ivankov@resolto.com>
2025-11-15 09:51:56 +0100

    Python native classes: initial batch

--- src/Python/desktop.py
+++ src/Python/desktop.py
@@ -1,10 +1,33 @@
-from typing import List
+from typing import Dict, List
 from category_object import CategoryObject
 from category_arrow import CategoryArrow
 
 class Desktop: 
     def __init__(self):
-        self._category_objects: List[CategoryObject]  = []
-        self._category_arrows: List["CategoryArrow"] = []
+        self._category_objects_arrows_dict: Dict[str, CategoryObject | CategoryArrow] = {}
 
+    @property
+    def category_objects(self):
+        return [v for v in self._category_objects_arrows_dict.values() if issubclass(v, CategoryObject)]
+    
+    @property
+    def category_arrows(self):
+        return [v for v in self._category_objects_arrows_dict.values() if issubclass(v, CategoryArrow)]
+    
+    def __getitem__(self, key: str):
+        return self._category_objects_arrows_dict[key]
+    
+    def __add__(self, obj_or_arr: CategoryObject | CategoryArrow | List[CategoryObject | CategoryArrow]):
+        if issubclass(obj_or_arr, CategoryObject) or (issubclass, obj_or_arr, CategoryArrow):
+            self._category_objects_arrows_dict[obj_or_arr.name] = obj_or_arr
+        elif issubclass(obj_or_arr, list):
+            for item in obj_or_arr:
+                self = self + item
+        else:
+            raise TypeError("Item to be added is neither a CategoryObject nor a CategoryArrow")
+
+    def post_set(self):
+        for v in self._category_objects_arrows_dict.values:
+            if (callable(getattr(v, "post_set"))):
+                v.post_set()
     
---
+++ src/Python/measurements/measurement.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+class Measurement:
+    
+    def __init__():
+        pass
+
+    @property
+    def measurement_name(self):
+        raise NotImplemented()
+    
+    @property
+    def measurement_type(self):
+        raise NotImplemented()
+    
+    @property
+    def measurement_value(self):
+        raise NotImplemented()
+    
